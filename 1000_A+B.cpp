9e3bd452a626be4a9af2937f0ea5081f2b667e58
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2021-12-27 15:11:41 +0900

    Add files via upload

---
+++ 1000_A+B.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+	int input1, input2, num;
+
+	cin >> input1 >> input2;
+
+	num = input1 + input2;
+
+	cout << num;
+
+	return 0;	
+}
