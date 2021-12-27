de559ab8c24cd7f1d0c66a82e09b489b10bf7bb4
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2021-12-27 21:30:49 +0900

    Add files via upload

---
+++ 10950_A+B-3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a,b,c;
+	cin >> a;
+
+	for (int i = 0; i < a; i++) {
+		cin >> b >> c;
+
+		cout << b + c << endl;
+	}
+	return 0;
+
+}
