016caa1297bd48e60aae48692d1ab0e51aee71ea
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-04-16 00:03:08 +0900

    Add files via upload

--- 1978_소수 찾기.cpp
+++ 1978_소수 찾기.cpp
@@ -1,27 +1,63 @@
 #include <iostream>
-#include <cmath>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
-int main() {
-	int num;
-	cin >> num;
-	int count = num;
-	int input[100] = { 0, };
 
-	for (int i = 0; i < num; i++) {
-		cin >> input[i];
-		if (input[i] == 1)
-			count--;
+vector <bool> premium(int n) {
+
+	vector <bool> v(n + 1);
+
+	v[0] = 1;
+	v[1] = 1;
+
+	for (int i = 2; i <= sqrt(n); i++) {
+		if (v[i] == 1) {
+			continue;
+		}
+		for (int j = 2*i; j <= n; j += i) {
+			v[j] = 1;
+		}
 	}
 
-	for(int i=0;i<num;i++)
-		for (int j = 2; j <= sqrt(input[i]); j++)
-		{
-			if (input[i] % j == 0) {
-				count--;
-				break;
-			}
+	return v;
+}
+
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n;
+
+	int* array = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> array[i];
+	}
+
+	sort(array, array + n);
+
+	int m = array[n - 1];
+	vector <bool> vt;
+
+	vt = premium(m);
+	
+	for (int i = 0; i < vt.size(); i++) {
+		cout << vt[i];
+	}
+
+	cout << "\n";
+
+	int count = 0;
+	for (int i = 0; i < n; i++) {
+		if (vt[array[i]] == 0) {
+			count++;
 		}
+	}
+
 	cout << count;
-	return 0;
 }
