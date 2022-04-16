5e66e2546c65de45987f89f65437224fcd3f3ce3
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-04-17 00:07:04 +0900

    Add files via upload

--- 1644_소수의 연속합.cpp
+++ 1644_소수의 연속합.cpp
@@ -1,61 +1,85 @@
+// 정수 하나를 입력 받아 하나 이상의 연속된 소수의 합으로 나타낼 수 있는 경우의 수
+
 #include <iostream>
 #include <vector>
 #include <cmath>
 using namespace std;
 
-int main(void) {
-	int n, rt;
-	vector <int> v;
 
-	cin >> n;
+vector <int> premium(int m) { //2와 m사이의 소수를 반환해주는 함수
 
-	if (n == 1) {
-		cout << "0";
-		return 0;
-	}
+	vector <bool> v(m + 1);
 
-	for (int i = 2; i <= n; i++) {
-		rt = sqrt(i);
-		if (rt == 1 && i != 1) {
-			v.push_back(i);
+	for (int i = 2; i <= sqrt(m); i++) {
+		if (v[i] == 1) {
 			continue;
 		}
-		if (i % 2) {
-			for (int j = 2; j <= rt; j++) {
-				if ((i % j) == 0)
-					break;
-				if (j == rt) {
-					v.push_back(i);
-				}
-			}
+		for (int j = 2 * i; j <= m; j += i) {
+			v[j] = 1;
+		}
+	} //소수면 0, 아니면 1
+
+	vector <int> vs;
+	for (int i = 2; i <= m; i++) {
+		if (v[i] == 0) {  //소수가 아닌 것들만
+			vs.push_back(i); //벡터 vs에 저장
 		}
 	}
-
-	int count = 0;
-	int start = 0, end = 0;
-	int sum = 0;
-
-	while (start <= end && end < v.size())
-	{
-		if (sum < n) {
-			sum += v[end];
-			end++;
-			continue;
-		}
-		else if (sum >= n) {
-			if (sum == n) {
-				count++;
-			}
-			sum -= v[start];
-			start++;
-			continue;
-		}
-	}
-	if (v[v.size() - 1] == n) {
-		count++;
-	}
-
-	cout << count;
+	return vs;
 }
 
 
+int count(vector <int> vs, int n) { // 벡터 vs 속 원소들을 이용하여 정수 n을 만드는 경우의 수를 count에 저장
+
+	int sum = 0;
+	int ptrA = -1;
+	int ptrB = -1;
+	int count = 0;
+
+	while (1) {
+		if (sum < n) {  //만약 합계가 정수보다 작다면
+			if (ptrB + 1 == vs.size()) { //포인터 B가 벡터 vs의 마지막 원소를 가르킨다면
+				break;
+			}
+			ptrB++; // 포인터 오른쪽으로 이동
+			sum += vs[ptrB];
+
+			if (sum == n) {
+				count++;
+			}
+			
+		}
+		else {
+			if (ptrA + 1 == vs.size()) { //포인터 A가 벡터 vs의 마지막 원소를 가르킨다면
+				break;
+			}
+
+			ptrA++; //포인터 오른쪽으로 이동
+			sum -= vs[ptrA];
+
+			if (sum == n) {
+				count++;
+			}
+		}
+	}
+	return count;
+}
+
+
+
+
+int main() {
+	ios_base::sync_with_stdio(0); //입출력 빠르게
+	cin.tie(0);
+	cout.tie(0);
+
+	int n;
+	cin >> n; //정수 n 입력받기
+
+	vector <int> vs = premium(n);
+
+	cout << count(vs, n);
+
+
+
+}
