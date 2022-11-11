e01176a3e971a6f1d9a8e76d0d62836efa45cb23
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-11-11 23:12:45 +0900

    Add files via upload

---
+++ 11727_2Xn 타일링.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[1001];
+
+int solution(int n) {
+
+	if (n == 1) {
+		return dp[1];
+	}
+	else if (n == 2) {
+		return dp[2];
+	}
+
+	if (dp[n]) {
+		return dp[n];
+	}
+	else {
+		return dp[n] = solution(n - 1) + 2 * solution(n - 2);
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	dp[1] = 1;
+	dp[2] = 3;
+
+	cout << solution(n);
+
+}
