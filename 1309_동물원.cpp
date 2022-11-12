282f4696ed9b92ee6ede33f33b2e2e649c9b59d9
youbeen2798 <62228401+youbeen2798@users.noreply.github.com>
2022-11-12 21:32:37 +0900

    Add files via upload

---
+++ 1309_동물원.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int dp[100001][3];
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	cin >> n;
+
+	dp[1][0] = 1; //¿ÞÂÊ º°
+	dp[1][1] = 1; //¿À¸¥ÂÊ º°
+	dp[1][2] = 1; //ºóÄ­ 2°³
+
+	for (int i = 2; i <= n; i++) {
+		dp[i][0] = (dp[i - 1][1] + dp[i-1][2]) % 9901;
+		dp[i][1] = (dp[i - 1][0] + dp[i - 1][2]) % 9901;
+		dp[i][2] = (dp[i - 1][0] + dp[i - 1][1] + dp[i - 1][2]) % 9901;
+	}
+
+	int ans = (dp[n][0] + dp[n][1] + dp[n][2]) %9901;
+	cout << ans;
+}
