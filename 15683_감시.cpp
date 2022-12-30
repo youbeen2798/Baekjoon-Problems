06950c83e74895a611a590f9d38d04fe0ed581f4
youbeenJeong <youbeen2798@naver.com>
2022-12-30 19:06:15 +0900

    Add files via upload

--- 15683_감시.cpp
+++ 15683_감시.cpp
@@ -1,119 +1,135 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int row;
-int col;
-int CAM_SIZE = 0;
-int office[8][8];
+int n, m;
+int arr[9][9];
+bool direct[9][9];
+int camera_num = 0; //Ä«¸Þ¶ó °³¼ö
 
-int answer = 100;
-struct CAM {
-	int x, y, type;
+int directions[6] = { 0, 4, 2, 4, 4, 1 };
+
+struct camera {
+	int camera_type;
+	int x;
+	int y;
 };
+camera cameras[8];
 
-CAM cam[8];
+int ans = 100;
 
-int rotation[5] = { 4,2,4,4,1 };
+void input() {
+	cin >> n >> m; //¼¼·Î Å©±â, °¡·Î Å©±â
 
-void update(int dir, CAM c) {
-
-	dir %= 4;
-
-	if (dir == 0) { //ì˜¤ë¥¸ìª½
-		for (int y = c.y + 1; y < col; y++) {
-			if (office[c.x][y] == 6) {
-				break;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+			if (1 <= arr[i][j] && arr[i][j] <= 5) {
+				cameras[camera_num] = { arr[i][j], i, j };
+				camera_num++;
 			}
-			office[c.x][y] = -1;
-		}
-	}
-	else if (dir == 1) { //ìœ„ìª½
-		for (int x = c.x - 1; x >= 0; x--) {
-			if (office[x][c.y] == 6) {
-				break;
-			}
-			office[x][c.y] = -1;
-		}
-	}
-	else if (dir == 2) { //ì™¼ìª½
-		for (int y = c.y - 1; y >= 0; y--) {
-			if (office[c.x][y] == 6) {
-				break;
-			}
-			office[c.x][y] = -1;
-		}
-	}
-	else if (dir == 3) { //ì•„ëž˜ìª½
-		for (int x = c.x + 1; x < row; x++) {
-			if (office[x][c.y] == 6) {
-				break;
-			}
-			office[x][c.y] = -1;
 		}
 	}
 }
 
-void copy(int backup[8][8], int origin[8][8]) {
-	for (int i = 0; i < 8; i++) {
-		for (int j = 0; j < 8; j++) {
+void watch(int dir, int x,int y) {
+	dir = dir % 4;
+
+	if (dir == 0) { //À§ÂÊ
+		for (int i = x - 1; i >= 0; i--) {
+			if (arr[i][y] == 6) {
+				break;
+			}
+			direct[i][y] = true;
+		}
+	}
+	else if (dir == 1) { //¿À¸¥ÂÊ
+		for (int i = y + 1; i < m; i++) {
+			if (arr[x][i] == 6) {
+				break;
+			}
+			direct[x][i] = true;
+		}
+	}
+	else if (dir == 2) { //¾Æ·¡ÂÊ
+		for (int i = x + 1; i < n; i++) {
+			if (arr[i][y] == 6) {
+				break;
+			}
+			direct[i][y] = true;
+		}
+	}
+	
+	else if (dir == 3) { //¿ÞÂÊ
+		for (int i = y - 1; i >= 0; i--) {
+			if (arr[x][i] == 6) {
+				break;
+			}
+			direct[x][i] = true;
+		}
+	}
+}
+
+void copy(bool backup[9][9], bool origin[9][9]) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
 			backup[i][j] = origin[i][j];
 		}
 	}
 }
+void solution(int num) {
 
-void solve(int cam_idx) {
-	
-
-	if (cam_idx == CAM_SIZE) {
-
+	if (num == camera_num) {
 		int w = 0;
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < col; j++) {
-				if (office[i][j] == 0) {
-					w++;
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (!direct[i][j] && arr[i][j] == 0) {
+				//	cout << "i: " << i << " j: " << j << "\n";
+					w++; 
 				}
 			}
 		}
-
-		if (answer > w) {
-			answer = w;
-		}
+	//	cout << "w: " << w << "\n";
+		ans = min(w, ans);
 		return;
 	}
-	int type = cam[cam_idx].type;
-	int backup[8][8];
 
-	for (int dir = 0; dir < rotation[type]; dir++) {
+	bool backup[9][9];
 
-		copy(backup, office);
+	//0¹ø Ä«¸Þ¶óÀÇ Å¸ÀÔ
+	for (int dir = 0; dir < directions[cameras[num].camera_type]; dir++) {
+		int camera_type = cameras[num].camera_type; //1¹øÀÌ¶ó °¡Á¤
 
-		
-		if (type == 0) {
-			update(dir, cam[cam_idx]);
+		copy(backup, direct);
+		//0: À§ÂÊ 1:¿À¸¥ÂÊ 2: ¾Æ·¡ÂÊ 3: ¿ÞÂÊ
+		if (camera_type == 1) {
+			watch(dir, cameras[num].x, cameras[num].y); //0, 1, 2, 3
 		}
-		else if (type == 1) {
-			update(dir, cam[cam_idx]);
-			update(dir + 2, cam[cam_idx]);
+		else if (camera_type == 2) { //0,2 //1,3
+			watch(dir, cameras[num].x, cameras[num].y);
+			watch(dir + 2, cameras[num].x, cameras[num].y);
 		}
-		else if (type == 2) {
-			update(dir, cam[cam_idx]);
-			update(dir + 1, cam[cam_idx]);
+		else if (camera_type == 3) { //0,1 //1,2 //2,3 //3,4
+			watch(dir, cameras[num].x, cameras[num].y);
+			watch(dir + 1, cameras[num].x, cameras[num].y); 
 		}
-		else if (type == 3) {
-			update(dir, cam[cam_idx]);
-			update(dir +1, cam[cam_idx]);
-			update(dir +2, cam[cam_idx]);
+		else if (camera_type == 4) { //0,1,2 //1,2,3 //2,3,0 //3,4,5
+			watch(dir, cameras[num].x, cameras[num].y);
+			watch(dir + 1, cameras[num].x, cameras[num].y);
+			watch(dir + 2, cameras[num].x, cameras[num].y);
 		}
-		else if (type == 4) {
-			update(dir, cam[cam_idx]);
-			update(dir + 1, cam[cam_idx]);
-			update(dir + 2, cam[cam_idx]);
-			update(dir + 3, cam[cam_idx]);
+		else if (camera_type == 5) {
+			watch(dir, cameras[num].x, cameras[num].y);
+			watch(dir + 1, cameras[num].x, cameras[num].y);
+			watch(dir + 2, cameras[num].x, cameras[num].y);
+			watch(dir + 3, cameras[num].x, cameras[num].y);
 		}
 
-		solve(cam_idx + 1);
-		copy(office, backup);
+		solution(num + 1);
+		copy(direct, backup);
 	}
 }
 int main() {
@@ -121,24 +137,7 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
-	cin >> row >> col;
-
-	for (int i = 0; i < row; i++) {
-		for (int j = 0; j < col; j++) {
-			cin >> office[i][j];
-
-			if (office[i][j] != 0 && office[i][j] != 6) {
-				cam[CAM_SIZE].x = i;
-				cam[CAM_SIZE].y = j;
-				cam[CAM_SIZE].type = office[i][j] - 1; //0
-
-				CAM_SIZE++;
-			}
-
-		}
-	}
-
-	solve(0);
-
-	cout << answer;
+	input();
+	solution(0);
+	cout << ans;
 }
