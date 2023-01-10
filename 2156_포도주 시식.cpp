ee2f0d1a2b849f5775386a41b0befe46357bea25
youbeenJeong <youbeen2798@naver.com>
2023-01-11 00:21:51 +0900

    Add files via upload

--- 2156_포도주 시식.cpp
+++ 2156_포도주 시식.cpp
@@ -5,25 +5,42 @@ using namespace std;
 int n;
 int arr[10001];
 int dp[10001];
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
 
+void solution() {
+
+	if (n == 1) {
+		cout << arr[1];
+		return;
+	}
+	else if (n == 2) {
+		cout << arr[1] + arr[2];
+		return;
+	}
+	dp[1] = arr[1];
+	dp[2] = dp[1] + arr[2];
+
+	for (int i = 3; i <= n; i++) {
+		dp[i] = max(max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]), dp[i - 1]);
+	}
+//	cout << "##dp[1]: " << dp[1] << "\n";
+//	for (int i = 0; i < n; i++) {
+//		cout << "i: " << i << " dp[i]: " << dp[i] << "\n";
+//	}
+	cout << dp[n ];
+}
+
+void input() {
 	cin >> n;
 
 	for (int i = 1; i <= n; i++) {
 		cin >> arr[i];
 	}
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-	dp[1] = arr[1];
-	dp[2] = arr[1] + arr[2];
-
-	for (int i = 3; i <= n; i++) {
-		dp[i] = max(max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]), dp[i - 1]);
-	}
-
-
-	cout << dp[n];
-
+	input();
+	solution();
 }
