1501f861b86bc0d7a0211d9b56526bce0a1a4ed6
youbeenJeong <youbeen2798@naver.com>
2023-01-11 15:49:31 +0900

    Add files via upload

--- 10844_쉬운 계단 수.cpp
+++ 10844_쉬운 계단 수.cpp
@@ -1,41 +1,40 @@
 #include <iostream>
-
-#define MAX 1000000000
+#define N 1000000000
 using namespace std;
 
-long long dp[101][10] = { 0, };
 int n;
-long long sum = 0;
+int dp[101][10];
 
-int main()
-{
-	cin >> n;
+void solution() {
 
+	dp[1][0] = 0;
 	for (int i = 1; i <= 9; i++) {
 		dp[1][i] = 1;
 	}
 
 	for (int i = 2; i <= n; i++) {
-
-		for (int j = 0; j < 10; j++) {
-			if (j == 0)
-				dp[i][0] = dp[i - 1][j + 1];
-			else if (j == 9)
-				dp[i][j] = dp[i - 1][j - 1];
-			else
-				dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
-
-			dp[i][j] %= MAX;
+		for (int j = 1; j <= 8; j++) {
+			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % N;
 		}
-
+		dp[i][9] = dp[i - 1][8] % N;
+		dp[i][0] = dp[i - 1][1] % N;
 	}
 
-
+	long ans = 0;
 	for (int i = 0; i <= 9; i++) {
-		sum = (sum + dp[n][i]) % MAX;
+		ans += dp[n][i];
 	}
 
-	cout << sum;
+	cout <<  ans % N;
+}
+void input() {
+	cin >> n;
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0); 
+	cout.tie(0);
 
-	return 0;
+	input();
+	solution();
 }
