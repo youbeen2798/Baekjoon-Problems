1ce73f326d997b810502ee35fb6c634ae6b1ad27
youbeenJeong <youbeen2798@naver.com>
2023-01-13 21:13:54 +0900

    Add files via upload

--- 2573_빙산.cpp
+++ 2573_빙산.cpp
@@ -1,74 +1,17 @@
 #include <iostream>
-#include <cstring>
 #include <queue>
-
 using namespace std;
 
 int n, m;
-bool ismelt = false; 
-int icecnt[301][301];
 int arr[301][301];
-
-bool visited[301][301];
+bool visited[301][301]; 
 
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
 
-void solution() {
-
-	memset(icecnt, 0, sizeof(icecnt)); //0으로 초기화
-	memset(visited, false, sizeof(visited));
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			int cnt = 0;
-			if (arr[i][j]) { //0이 아니면	
-				for (int k = 0; k < 4; k++) {
-					int nx = i + dx[k]; //위 아래 왼쪽 오른쪽
-					int ny = j + dy[k]; //위 아래 왼쪽 오른쪽
-					
-					//해당 방향이 범위를 유지하면서 얼음이라면
-					if (0 <= nx && nx < n && 0 <= ny && ny < m && arr[nx][ny] != 0) {
-						cnt++;
-					}
-				}
-				icecnt[i][j] = 4 - cnt; //cnt란 주변 얼음이 없는 개수
-			}
-		}
-	}
-}
-
-
-void melt() {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] > 0 && icecnt[i][j] > 0) {
-				ismelt = true;
-				if (arr[i][j] <= icecnt[i][j]) {
-					arr[i][j] = 0;
-				}
-				else {
-					arr[i][j] -= icecnt[i][j];
-				}
-			}
-		}
-	}
-}
-
-//모두 다 녹았는지 확인하는 메소드
-bool allmelted() {
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (arr[i][j] != 0) {
-				return false;
-			}
-		}
-	}
-	return true;
-}
 void bfs(int x, int y) {
-	queue<pair<int, int>> q;
+
+	queue < pair<int, int>> q;
 	q.push({ x,y });
 	visited[x][y] = true;
 
@@ -82,13 +25,120 @@ void bfs(int x, int y) {
 			int nx = a + dx[i];
 			int ny = b + dy[i];
 
-			if (0 <= nx && nx < n && 0 <= ny && ny < m && arr[nx][ny] != 0 && !visited[nx][ny]) {
+			if (0 <= nx && nx < n && 0 <= ny && ny < m && !visited[nx][ny] && arr[nx][ny] > 0) {
 				visited[nx][ny] = true;
 				q.push({ nx,ny });
 			}
 		}
 	}
 }
+
+bool over_two() {
+
+	int ans = 0;
+
+	memset(visited, false, sizeof(visited));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] > 0 && !visited[i][j]) {
+				bfs(i, j);
+				ans++;
+			}
+		}
+	}
+
+	if (ans >= 2) {
+		return true;
+	}
+	return false;
+}
+
+int check_around(int x, int y) {
+
+	int tmp = 0;
+	for (int i = 0; i < 4; i++) {
+		int nx = x + dx[i];
+		int ny = y + dy[i];
+
+		if (0 <= nx && nx < n && 0 <= ny && ny < m && arr[nx][ny] == 0) {
+			tmp++;
+		}
+	}
+	return tmp;
+}
+
+void copy(int tmp[301][301]) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			arr[i][j] = tmp[i][j];
+		}
+	}
+}
+void ice_melt() {
+
+	int tmp[301][301] = { 0 };
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] > 0) {
+				tmp[i][j] = arr[i][j] - check_around(i, j);
+
+				if (tmp[i][j] < 0) {
+					tmp[i][j] = 0;
+				}
+			}
+		}
+	}
+	copy(tmp);
+}
+
+void print() {
+	
+	cout << "######" << "\n";
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << arr[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+bool all_melt() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] > 0) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+void solution() {
+
+	int time_answer = 0;
+
+	while (true) {
+
+		if (over_two()) { //빙산이 2개 이상이면
+			cout << time_answer;
+			exit(0);
+		}
+		if (all_melt()) {
+			cout << 0;
+			exit(0);
+		}
+
+		ice_melt(); //얼음 녹음
+
+		print();
+		time_answer++; //시간 1개 늘어남
+
+//		print(); //출력
+	}
+}
+
 void input() {
 	cin >> n >> m; //행의 개수와 열의 개수
 
@@ -97,6 +147,9 @@ void input() {
 			cin >> arr[i][j];
 		}
 	}
+
+//	print(); //출력
+
 }
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -104,48 +157,5 @@ int main() {
 	cout.tie(0);
 
 	input();
-
-	int ans = 0;
-	while (true) {
-
-		ans++;
-		int bfsCnt = 0;
-
-		solution();
-		melt();
-
-		//다 녹은 다음에 확인
-		memset(visited, false, sizeof(visited)); //방문 안한걸로 모두 처리
-
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (arr[i][j] != 0 && !visited[i][j]) { //방문하지 않았고 얼음이 있다면
-					bfsCnt++; //bfs가 불린 횟수
-					bfs(i, j);
-				}
-			}
-		}
-
-		cout << "---------------------------" << "\n";
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				cout << arr[i][j] << " ";
-			}
-			cout << "\n";
-		}
-
-		//	cout << "현재 bfsCnt: " << bfsCnt << " ans: " << ans << "\n";
-
-		if (bfsCnt >= 2) {
-			break;
-		}
-		else if (allmelted()) { //다 녹았는데 분리되지 않았
-			cout << "0";
-			exit(0);
-		}
-	}
-
-	cout << ans;
-	
-
+	solution();
 }
