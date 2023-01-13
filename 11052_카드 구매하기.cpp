1fb0c6681c14bd22388a434f30e335c1e21a8036
youbeenJeong <youbeen2798@naver.com>
2023-01-13 15:41:40 +0900

    Add files via upload

--- 11052_카드 구매하기.cpp
+++ 11052_카드 구매하기.cpp
@@ -1,24 +1,35 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
 int n;
-int dp[1001];
+int price[10001];
+int dp[10001];
 
-int arr[10001];
-int main() {
-	cin >> n;
-
-	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
-	}
+void solution() {
 
 	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			dp[i] = max(dp[i], arr[j] + dp[i - j]); 
+		dp[i] = price[i]; //dp[3] = price[3]
+		for (int j = 1; j <= i / 2; j++) {
+			dp[i] = max(dp[i], dp[j] + dp[i - j]);
 		}
 	}
-	
+
 	cout << dp[n];
+}
+
+void input() {
+	cin >> n; //4
+
+	for (int i = 1; i <= n; i++) {
+		cin >> price[i]; //1 5 6 7
+	}
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	input();
+	solution();
 }
