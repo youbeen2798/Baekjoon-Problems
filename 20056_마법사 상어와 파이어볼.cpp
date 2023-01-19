997eb11f8fdb314577e9cfdad3b7c30cdd34d5a9
youbeenJeong <youbeen2798@naver.com>
2023-01-20 00:16:39 +0900

    Add files via upload

--- 20056_마법사 상어와 파이어볼.cpp
+++ 20056_마법사 상어와 파이어볼.cpp
@@ -1,242 +1,198 @@
 #include <iostream>
 #include <vector>
-#include <set>
+
 using namespace std;
 
+int fire_ball_size_num = 0;
+
+struct fireball {
+	int mi; //파이어볼 질량
+	int si; //파이어볼 속력
+	int di; //파이어볼 방향
+};
 int n, m, k;
 
-struct animal {
-	int x; //x좌표(행)
-	int y; //y좌표(열)
-	int m; //질량
-	int s; //속도
-	int d; //방향
-};
+vector<fireball> arr[51][51];
+vector<fireball> tmp_arr[51][51];
 
+int dx[8] = { -1,-1,0,1,1,1,0,-1 };
+int dy[8] = { 0,1,1,1,0,-1,-1,-1 };
 
-vector<animal> v[51][51];
-
-pair<int,int> move(int ri, int ci, int si, int di) {
-
-	if (di == 0) { //북
-		ri -=  si;
-	}
-	else if (di == 1) { //동북
-		ri -=  si;
-		ci +=  si;
-	}
-	else if (di == 2) { //동
-		ci +=   si;
-	}
-	else if (di == 3) { //남동
-		ri += si;
-		ci +=  si;
-	}
-	else if (di == 4) { //남
-		ri += si;
-	}
-	else if (di == 5) { //남서
-		ri += si;
-		ci -=  si;
-	}
-	else if (di == 6) { //서
-		ci -= si;
-	}
-	else if (di == 7) { //북서
-		ri -= si;
-		ci -= si;
-	}
-
-	if (ri > n) {
-		if (ri % n == 0) {
-			ri = n;
-		}
-		else {
-			ri %= n;
+pair<int, int> check_over_n(int new_ri, int new_ci) {
+	if (new_ri > n) {
+		new_ri %= n;
+		if (new_ri == 0) {
+			new_ri = n;
 		}
 	}
-	else if (ri == 0) {
-		ri = n;
+	else if (new_ri == 0) {
+		new_ri = n;
 	}
-	else if (ri < 0) {
-		ri = n - (-1 * ri) % n;
+	else if (new_ri < 0) {
+		new_ri = n - abs(new_ri) % n;
 	}
 
-	if (ci > n) {
-		if (ci % n == 0) {
-			ci = n;
-		}
-		else {
-			ci %= n;
+	if (new_ci > n) {
+		new_ci %= n;
+		if (new_ci == 0) {
+			new_ci = n;
 		}
 	}
-	else if (ci == 0) {
-		ci = n;
+	else if (new_ci == 0) {
+		new_ci = n;
 	}
-	else if (ci < 0) {
-		ci = n - (-1 * ci) % n;
+	else if (new_ci < 0) {
+		new_ci = n - abs(new_ci) % n;
 	}
 
-	return { ri,ci };
-
+	return { new_ri, new_ci };
 }
 
-void combine() {
+void real_move_fireball(int ri, int ci) {
+	//위치 (ri, ci)의 파이어볼을 si 속력으로 di만큼 이동시키기
+	vector<fireball> tmp_fireball = arr[ri][ci];
+
+	for (int i = 0; i < tmp_fireball.size(); i++) {
+		int mi = tmp_fireball[i].mi;
+		int si = tmp_fireball[i].si;
+		int di = tmp_fireball[i].di;
+
+		int new_ri = ri + si * dx[di];
+		int new_ci = ci + si * dy[di];
+
+		pair<int, int> p = check_over_n(new_ri, new_ci);
+		tmp_arr[p.first][p.second].push_back({ mi, si, di });
+	}
+	
+}
+void move_fireballs() {
+	//파이어볼 이동시키기
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			tmp_arr[i][j].clear();
+		}
+	}
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
-
-			if (v[i][j].size() >= 2) {
-				int mass_sum = 0; //질량 합
-				int speed_sum = 0; //속도 합
-
-				bool odd_speed = true; //모두 홀수라면 참
-				bool even_speed = true; //모두 짝수라면 참
-
-				for (auto anim : v[i][j]) {
-
-					if (anim.d % 2 == 0) {
-						odd_speed = false;
-					}
-					else if (anim.d % 2 != 0) {
-						even_speed = false;
-					}
-
-					mass_sum += anim.m;
-					speed_sum += anim.s;
-				}
-
-				mass_sum /= 5;
-
-				if (mass_sum == 0) {
-					v[i][j].clear();
-					continue;
-				}
-
-				speed_sum /= v[i][j].size();
-				v[i][j].clear();
-
-				if (odd_speed || even_speed) {
-					for (int k = 0; k <= 3; k++) {
-						animal a;
-						a.x = i;
-						a.y = j;
-						a.m = mass_sum;
-						a.s = speed_sum;
-						a.d = k * 2;
-						v[i][j].push_back(a);
-					}
-				}
-				else {
-					for (int k = 0; k <= 3; k++) {
-						animal a;
-						a.x = i;
-						a.y = j;
-						a.m = mass_sum;
-						a.s = speed_sum;
-						a.d = k * 2 + 1;
-						v[i][j].push_back(a);
-					}
-				}
-
-
+			if (arr[i][j].size() > 0) {
+				//만약 파이어볼이 존재한다면
+				real_move_fireball(i, j);
+				arr[i][j].clear(); //원래 있던 파이어볼 제거
+				//(i,j) 위치에 있는 fireball 이동시키기
+			}
+		}
+	}
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (tmp_arr[i][j].size() > 0) {
+				arr[i][j].insert(arr[i][j].end(), tmp_arr[i][j].begin(), tmp_arr[i][j].end());
 			}
 		}
 	}
 }
 
-void movemove() {
+void over_two_fireball(int row, int col) {
 
-	vector<animal> new_v[51][51];
+	int total_weight = 0;
+	int total_speed = 0;
+	bool all_odd = true;
+	bool all_even = true;
+
+	vector<fireball> v = arr[row][col]; //특정 행에 있는 파이어볼 모음
+	arr[row][col].clear();
+
+	for (int i = 0; i < v.size(); i++) {
+		total_weight += v[i].mi; //질량 더하기
+		total_speed += v[i].si; //속도 더하기
+
+		if (v[i].di % 2 == 0) {
+			//파이어볼 방향이 짝수라면
+			all_odd = false; //모두 홀수인건 false
+		}
+		else if (v[i].di % 2 != 0) {
+			//파이어볼 방향이 홀수라면
+			all_even = false; //모두 짝수인건 false 
+		}
+	}
+
+	int weight = total_weight / 5; //질량은 질량합 / 5
+	int speed = total_speed / v.size(); //속력의 합 / 파이어볼 개수
+
+	if (weight == 0) { //만약 질량이 0인 파이어볼이라면 소멸
+		return; 
+	}
+	if (all_odd || all_even) { 
+		//합쳐지는 파이어볼 방향이 모두 홀수이거나 모두 짝수이면
+		for (int i = 0; i < 4; i++) {
+			arr[row][col].push_back({ weight, speed, i * 2 });
+		}
+	}
+	else {
+		for (int i = 0; i < 4; i++) {
+			arr[row][col].push_back({ weight, speed, i * 2 + 1 });
+		}
+	}
+}
+
+void after_move() {
+	//이동이 모두 끝난뒤
 
 	for (int i = 1; i <= n; i++) {
 		for (int j = 1; j <= n; j++) {
-
-			if (v[i][j].size() > 0) { //이동하기
-
-				for (auto anim : v[i][j]) {
-
-					auto b = move(anim.x, anim.y, anim.s, anim.d); //이동 후 좌표
-
-					animal new_animal;
-
-					new_animal.x = b.first;
-					new_animal.y = b.second;
-					new_animal.s = anim.s;
-					new_animal.d = anim.d;
-					new_animal.m = anim.m;
-
-					new_v[b.first][b.second].push_back(new_animal);
-				}
-
-				v[i][j].clear();
+			//만약 2개 이상의 파이어볼이 있는 칸이라면
+			if (arr[i][j].size() >= 2) {
+				over_two_fireball(i,j);
 			}
 		}
 	}
+}
 
-	//이동시킨 동물 추가하기
-	for (int i = 0; i <= n; i++) {
-		for (int j = 0; j <= n; j++) {
-			if (new_v[i][j].size() > 0) {
-				for (animal a : new_v[i][j]) {
-					v[i][j].push_back(a);
+int get_weight_sum() {
+	//남아있는 파이어볼 질량 합 구하기
+
+	int weight = 0;
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			if (arr[i][j].size() > 0) {
+				for (int k = 0; k < arr[i][j].size(); k++) {
+					weight += arr[i][j][k].mi;
 				}
 			}
 		}
 	}
 
+	return weight;
+}
+void solution() {
+
+	for (int i = 0; i < k; i++) {
+		move_fireballs();
+		after_move();
+	}
+	
+	cout << get_weight_sum();
+}
+void input() {
+	cin >> n >> m >> k;
+
+	for (int i = 0; i < m; i++) {
+		int ri; //파이어볼 행 위치
+		int ci; //파이어볼 열 위치
+		int mi; //질량
+		int si; //속력
+		int di; //방향
+		cin >> ri >> ci >> mi >> si >> di;
+		arr[ri][ci].push_back({ mi,si,di });
+	}
 }
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-
-	
-	int animalSize = 0;
-
-	cin >> n >> m >> k;
-
-	for (int i = 0; i < m; i++) { //처음 이동시키기
-		int ri, ci, mi, si, di;
-
-		cin >> ri >> ci >> mi >> si >> di; 
-
-		auto a = move(ri, ci, si, di); //이동
-
-		animal ani;
-
-		ani.x = a.first; //이동 후 x 좌표
-		ani.y = a.second;//이동 후 y 좌표
-		ani.m = mi;
-		ani.s = si;
-		ani.d = di;
-
-		v[ani.x][ani.y].push_back(ani);
-	}
-
-	combine();
-
-	if (k >= 2) {
-		for (int i = 0; i < k - 1; i++){
-			movemove();
-			combine();
-		}
-	}
-	
-
-	int answer = 0;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			if (v[i][j].size() > 0) {
-				for (auto anim : v[i][j]) {
-					answer += anim.m;
-				}
-			}
-		}
-	}
-	cout <<  answer << "\n";
-	
-
-
+	input();
+	solution();
 }
