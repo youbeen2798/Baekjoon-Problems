aa0de489231c9ea58e4cc7d6504e9102f3c244a4
youbeenJeong <youbeen2798@naver.com>
2023-01-21 23:10:15 +0900

    Add files via upload

--- 16234_인구 이동.cpp
+++ 16234_인구 이동.cpp
@@ -1,100 +1,115 @@
 #include <iostream>
 #include <queue>
-#include <vector>
+#include <cstring>
+#include <set>
 using namespace std;
 
 int n, l, r;
-int arr[101][101];
-bool visited[101][101]= { false, };
+
+int arr[51][51];
+int tmp[51][51];
+bool visited[51][51];
+int times = 0;
 
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
-int sum;
-
-queue <pair<int, int>> q;
-
-vector <pair<int, int>> v;
+bool changed = false;
 
 void bfs(int x, int y) {
 
-	sum += arr[x][y];
-	q.push(make_pair(x, y));
-	visited[x][y] = 1;
-	v.push_back(make_pair(x, y));
+	set<pair<int, int>> s;
+
+	queue <pair<int, int>> q;
+	visited[x][y] = true;
+
+	int total_num = 0;
+
+	q.push({ x,y });
+	s.insert({ x,y });
+	total_num += arr[x][y];
 
 	while (!q.empty()) {
-
 		int a = q.front().first;
 		int b = q.front().second;
+		int num1 = arr[a][b];
 
 		q.pop();
 
 		for (int i = 0; i < 4; i++) {
 			int nx = a + dx[i];
 			int ny = b + dy[i];
+			int diff = abs(num1 - arr[nx][ny]); 
 
-			if (0 <= nx && nx < n && 0 <= ny && ny < n
-				&& visited[nx][ny] == 0 && (abs(arr[nx][ny] - arr[a][b]) >= l)
-				&& (abs(arr[nx][ny] - arr[a][b]) <= r)) 
-			{
-				sum += arr[nx][ny];
-				v.push_back(make_pair(nx, ny));
-				visited[nx][ny] = 1;
-				q.push(make_pair(nx, ny));
+			if (0 <= nx && nx < n && 0 <= ny && ny < n && !visited[nx][ny] && l <= diff && diff <= r) {
+				visited[nx][ny] = true;
+				q.push({ nx,ny });
+				s.insert({ nx,ny });
+				total_num += arr[nx][ny];
 			}
-
 		}
 	}
 
-}
-int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
+	int average = total_num / s.size();
+	
+	for (auto p : s) {
+		tmp[p.first][p.second] = average;
+	}
 
-	cin >> n >> l >> r;
+}
+
+bool is_changed() {
+	//복사하면서 바뀌었는지 확인
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (tmp[i][j] != arr[i][j]) {
+				arr[i][j] = tmp[i][j];
+				changed = true;
+			}
+		}
+	}
+	return changed;
+}
+void solution() {
+	//두 나라의 인구 차이가 L명 이상 R명 이하라면, 두 나라가 공유하는 국경선을 오늘 하루동안 염
+
+
+	while (true) {
+		times++;
+		changed = false;
+		memset(visited, false, sizeof(visited));
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (!visited[i][j]) {
+					bfs(i, j);
+				}
+			}
+		}
+
+		if (!is_changed()) {
+			//바뀌지 않았으면
+			break;
+		}
+	}
+	cout <<  times - 1;
+		
+
+}
+
+void input() {
+	cin >> n >> l >> r; 
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> arr[i][j];
 		}
 	}
+}
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-	int answer = 0;
-	while (1) {
-
-		int cnt = 0;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (visited[i][j] == 0) {
-					v.clear();
-					sum = 0;
-					bfs(i, j);
-
-					if (v.size() > 1) {
-						cnt++;
-					}
-					int k = sum / v.size();
-					for (int i = 0; i < v.size(); i++) {
-						arr[v[i].first][v[i].second] = k;
-					}
-
-				}
-			}
-		}
-		
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				visited[i][j] = 0;
-			}
-		}
-
-
-		if (cnt == 0) {
-			break;
-		}
-		answer++;
-	}
-	
-	cout << answer << "\n";
+	input();
+	solution();
 }
