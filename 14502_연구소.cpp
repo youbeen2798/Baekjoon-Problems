ece50b82263b007b9080e8f45c5043ca0875743f
youbeenJeong <youbeen2798@naver.com>
2023-01-24 13:35:11 +0900

    Add files via upload

--- 14502_연구소.cpp
+++ 14502_연구소.cpp
@@ -1,104 +1,118 @@
 #include <iostream>
+#include <vector>
+#include <set>
 #include <queue>
 using namespace std;
 
-int n, m;
-int c = 0;
-int map[8][8];
-int tmp[8][8];
-
+int n, m; // 세로 크기, 가로 크기
+int arr[9][9];
+bool visited[9][9];
+int real_ans = 0;
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
+int original_empty_room_size = 0;
 
-int mini_cnt = 65;
+vector<pair<int, int>> empty_room;
 
-void mapCopy(int(*a)[8], int(*b)[8]) {
+void input() {
+
+	cin >> n >> m;
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			b[i][j] = a[i][j];
+			cin >> arr[i][j];
+			if (arr[i][j] == 0) { //빈칸
+				empty_room.push_back({ i,j });
+			}
 		}
 	}
+
+	original_empty_room_size = empty_room.size();
 }
 
-void bfs() {
-	int ans[8][8];
-	mapCopy(tmp, ans);
+ int bfs(int x, int y) {
+	queue<pair<int, int>> q;
 
-	queue <pair<int, int>> q;
-
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (ans[i][j] == 2) {
-				q.push(make_pair(i, j));				
-			}			
-		}
-	}
+	q.push({ x,y });
+	visited[x][y] = true;
+	int tmp_ans = 0;
 
 	while (!q.empty()) {
-		int x = q.front().first;
-		int y = q.front().second;
+		int a = q.front().first;
+		int b = q.front().second;
+
 		q.pop();
 
-		for (int p = 0; p < 4; p++) {
-			int nx = x + dx[p];
-			int ny = y + dy[p];
+		for (int i = 0; i < 4; i++) {
+			int nx = a + dx[i];
+			int ny = b + dy[i];
 
-			if ((0 <= nx) && (nx < n) && (0 <= ny) && (ny < m) && (ans[nx][ny] == 0)) {
-				ans[nx][ny] = 2;
-				q.push(make_pair(nx, ny));
-			}
-		}
-	}
-
-	int cnt = 0;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			if (ans[i][j] == 0) {
-				cnt++;
-			}
-		}
-	}
-
-	c = max(c, cnt);
-}
-void wall(int cnt) {
-	if (cnt == 3) {
-		bfs();
-		return;
-	}
-	else {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
-				if (tmp[i][j] == 0) {
-					tmp[i][j] = 1;
-					wall(cnt + 1);
-					tmp[i][j] = 0;
+			if (0 <= nx && nx < n && 0 <= ny && ny < m && !visited[nx][ny] && arr[nx][ny] != 1) {
+				q.push({ nx,ny });
+				visited[nx][ny] = true;
+				if (arr[nx][ny] == 0) { //빈칸이면
+					tmp_ans++;
 				}
 			}
 		}
 	}
-}
 
-int main() {
-	cin >> n >> m; //세로 크기 // 가로크기
-	
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-		}
+	return tmp_ans;
+}
+void check(vector<pair<int,int>> comb) {
+
+	for (int i = 0; i < comb.size(); i++) {
+		arr[comb[i].first][comb[i].second] = 1; //빈 칸 벽으로 만들기
 	}
 
+	memset(visited, false, sizeof(visited));
+
+	int ans = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			if (map[i][j] == 0) {
-				mapCopy(map, tmp); //map -> tmp로 배열 복사
-				tmp[i][j] = 1;
-				wall(1);
-				tmp[i][j] = 0;
+			if (arr[i][j] == 2 && !visited[i][j]) {
+				ans += bfs(i, j);
 			}
 		}
 	}
 	
-	cout << c;
+	int safe_zone_num = original_empty_room_size - ans - 3;
+	 
+	real_ans = max(real_ans, safe_zone_num);
+
+	for (int i = 0; i < comb.size(); i++) {
+		arr[comb[i].first][comb[i].second] = 0; //벽 다시 빈칸 만들기
+	}
+
+}
+void Combination(vector<pair<int, int>> arr, vector<pair<int, int>> comb, int r, int idx, int depth) {
+
+	if (r == 0) {
+		check(comb);
+	}
+	else if (arr.size() == depth) {
+		return;
+	}
+	else {
+		comb[idx] = arr[depth];
+
+		Combination(arr, comb, r - 1, idx + 1, depth + 1);
+		Combination(arr, comb, r, idx, depth + 1);
+	}
+}
+void solution() {
+
+	vector<pair<int, int>> comb(3);
+
+	Combination(empty_room, comb, 3, 0, 0); 
+	cout << real_ans;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	input();
+	solution();
 }
