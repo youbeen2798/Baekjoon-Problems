2ea5d07c844095831b05336435d1d944f856c7c5
youbeenJeong <youbeen2798@naver.com>
2023-01-27 02:40:13 +0900

    Add files via upload

--- 17142_연구소3.cpp
+++ 17142_연구소3.cpp
@@ -1,40 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <queue>
 #include <cstring>
+#include <queue>
+#include <set>
+#include <vector>
 
 using namespace std;
 
-int n, m;
-int board[51][51];
-int distances[51][51];
-
-
-bool room[51][51];
+int n; //연구소의 크기
+int m; //놓을 수 있는 바이러스 개수
+int arr[51][51];
+int dist[51][51];
 bool visited[51][51];
-
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
-int virus_size = 0;
-int answer = 3000;
+int real_ans = 999999999;
+int tmp_ans = 0;
+set<pair<int, int>> now_virus;
 
-bool impossible = true;
+vector<pair<int, int>> virus;
 
-vector <pair<int, int>> virus; //바이러스 위치 저장
+void bfs(int x, int y) {
+	
+	memset(visited, false, sizeof(visited));
 
-
-
-void bfs(int x, int y) { //바이러스 있는 곳부터 시작
-
-
-	queue <pair<int, int>> q; //0 0
-
+	queue<pair<int, int>> q;
 	q.push({ x,y });
-	distances[x][y] = 0;
 	visited[x][y] = true;
 
 	while (!q.empty()) {
-
 		int a = q.front().first;
 		int b = q.front().second;
 
@@ -44,109 +37,109 @@ void bfs(int x, int y) { //바이러스 있는 곳부터 시작
 			int nx = a + dx[i];
 			int ny = b + dy[i];
 
-			if (0 <= nx && nx < n && 0 <= ny && ny < n) {
-				if (visited[nx][ny] == false && board[nx][ny] != 1) {
-					q.push({ nx,ny });
-					visited[nx][ny] = true;
-					distances[nx][ny] = min(distances[a][b] + 1, distances[nx][ny]);
+			if (0 <= nx && nx < n && 0 <= ny && ny < n && !visited[nx][ny] && ((arr[nx][ny] == 0 )|| (arr[nx][ny] == 2 && now_virus.find({nx,ny}) == now_virus.end()))) {
+				visited[nx][ny] = true;
+
+				if (dist[nx][ny] == 0) {
+					dist[nx][ny] = dist[a][b] + 1;
 				}
+				else {
+					dist[nx][ny] = min(dist[nx][ny], dist[a][b] + 1);
+				}
+				q.push({ nx,ny });
 			}
 		}
 	}
-
 }
 
+bool check_all_virus(){
 
-void solve(vector <pair<int, int>> now_virus) {
-
-	for (int s = 0; s < n; s++) {
-		for (int t = 0; t < n; t++) {
-			distances[s][t] = 999999;
-		}
-	}
-
-	for (auto i : now_virus) {
-		memset(visited, false, sizeof(visited));
-
-		bfs(i.first, i.second);
-
-
-	}
-
-	int tmp_answer = -1;
+	tmp_ans = 0;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			if (distances[i][j] == 999999 && board[i][j] == 0) {
-				impossible = true;
-				return;
+
+			//바이러스이면서 비활성바이러스면
+			if (arr[i][j] == 2 && now_virus.find({ i,j }) == now_virus.end()) {
+				continue;
 			}
-
-			if (distances[i][j] != 999999 && board[i][j] == 0) {
-				if (tmp_answer < distances[i][j]) {
-					tmp_answer = distances[i][j];
-				}
+			tmp_ans = max(dist[i][j], tmp_ans);
+			if (arr[i][j] == 2 || arr[i][j] == 1 || dist[i][j] != 0) {
 			}
-
-
+			else {
+				return false;
+			}
 		}
 	}
-
-	impossible = false;
-	answer = min(answer, tmp_answer);
-
-
-
-
+	return true;
 }
 
-void combination(vector <int> arr, vector <int> comb, int r, int index, int depth) {
+void mini_solution(vector<pair<int, int>> comb) {
+
+	memset(dist, 0, sizeof(dist));
+
+	for (int i = 0; i < comb.size(); i++) {
+		bfs(comb[i].first, comb[i].second);
+	}
+
+	if (check_all_virus()) {
+		real_ans = min(real_ans, tmp_ans);
+	}
+}
+
+void Combination(vector<pair<int, int>> arr, vector<pair<int, int>> comb, int r, int idx, int depth) {
 	if (r == 0) {
-
-		vector <pair<int, int>> now_virus;
-
-		for (int i = 0; i < comb.size(); i++) { // 0 3 4
-			now_virus.push_back({ virus[comb[i]].first, virus[comb[i]].second });
+	//	cout << "###comb" << "\n";
+		now_virus.clear();
+		for (int i = 0; i < comb.size(); i++) {
+			now_virus.insert({ comb[i].first, comb[i].second });
+	//		cout << comb[i].first << " , " << comb[i].second << "\n";
 		}
-		solve(now_virus);
+		tmp_ans = 0;
+		mini_solution(comb);
+
+		if (check_all_virus()) {
+			real_ans = min(tmp_ans, real_ans);
+		}
 	}
 	else if (depth == arr.size()) {
 		return;
 	}
 	else {
+		comb[idx] = arr[depth];
 
-		//arr[depth]를 뽑는 경우
-		comb[index] = arr[depth];
-		combination(arr, comb, r - 1, index + 1, depth + 1);
+		Combination(arr, comb, r - 1, idx + 1, depth + 1);
 
-		//arr[depth]를 뽑지 않는 경우
-		combination(arr, comb, r, index, depth + 1);
+		Combination(arr, comb, r, idx, depth + 1);
 	}
+}
+void solution() {
+
+	vector<pair<int, int>> comb(m);
+
+	Combination(virus, comb, m, 0, 0);
+	
+	if (real_ans == 999999999) {
+		cout << -1;
+		exit(0);
+	}
+	cout << real_ans;
 }
 
 void input() {
 
-	cin >> n >> m;
-
-	bool zerocontain = false;
+	cin >> n >> m; //연구소 크기, 놓을 수 있는 바이러스 개수
 
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
-			if (board[i][j] == 2) {
-				virus_size++; //5
-				virus.push_back({ i,j }); //바이러스 위치 저장
+			cin >> arr[i][j];
+			if (arr[i][j] == 2) {
+				virus.push_back({ i,j });
 			}
-			if (board[i][j] == 0) {
-				zerocontain = true;
-			}
+			//0은 빈칸
+			//1은 벽
+			//2는 바이러스를 놓을 수 있는 위치
 		}
 	}
-
-	if (!zerocontain) { //만약 빈방이 없으면
-		cout << 0;
-		exit(0);
-	}
-
 }
 int main() {
 	ios_base::sync_with_stdio(0);
@@ -154,21 +147,16 @@ int main() {
 	cout.tie(0);
 
 	input();
-
-	vector <int> arr;
-
-	for (int i = 0; i < virus_size; i++) {
-		arr.push_back(i); //0 1 2 3 4
-	}
-
-	vector <int> comb(m);
-	combination(arr, comb, m, 0, 0);
-
-	if (answer == 3000) {
-		cout << -1;
-	}
-	else {
-		cout << answer;
-	}
+	solution();
 }
-
+/*
+8 3
+2 0 0 0 1 1 0 0
+0 0 1 0 1 2 0 0
+0 1 1 0 1 0 0 0
+0 1 0 0 0 0 0 0
+0 0 0 2 0 1 1 0
+0 1 0 0 0 0 0 0
+2 1 0 0 0 0 2 0
+0 0 0 0 0 0 0 0
+*/
