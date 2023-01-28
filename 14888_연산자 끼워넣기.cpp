b8c5096f88cd2903c5a7506beab6d60da94ab93e
youbeenJeong <youbeen2798@naver.com>
2023-01-29 03:04:09 +0900

    Add files via upload

--- 14888_연산자 끼워넣기.cpp
+++ 14888_연산자 끼워넣기.cpp
@@ -1,116 +1,107 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 int n;
-vector <int> num; // 5 6
-vector <int> operatorr; 
-vector <int> results;
+int arr[12];
 
+int operators[12];
+int first_num;
+
+int plus_num, minus_num, °öÇÏ±â_num, ³ª´©±â_num;
+//´õÇÏ±â, »©±â, °öÇÏ±â, ³ª´©±â
+// 1 , 2 , 3 , 4
+
+vector<int> v;
+vector<long> answers;
+
+void mini_solution(vector<int> v) {
+
+	int idx = 0;
+	for (auto tmp : v) {
+		operators[idx++] = tmp;
+	}
+
+//	cout << "n: " << n << "\n";
+
+//	cout << first_num << " ";
+
+	long result = first_num;
+	for (int i = 0; i < n - 1; i++) {
+		if (operators[i] == 1) { //µ¡¼ÀÀÌ¸é
+			result += arr[i];
+		}
+		else if (operators[i] == 2) { //»¬¼ÀÀÌ¸é
+			result -= arr[i];
+		}
+		else if (operators[i] == 3) { //°ö¼ÀÀÌ¸é
+			result *= arr[i];
+		}
+		else { //³ª´°¼ÀÀÌ¸é
+			if (result < 0) { //À½¼ö¸é
+				result = abs(result) / arr[i] * -1;
+				continue;
+			}
+			result = result / arr[i];
+		}
+//		cout << operators[i] << " " <<  arr[i] << " ";
+	}
+
+	answers.push_back(result);
+//	cout << "##result: " << result << "\n";
+//	cout << "\n";
+}
+
+void solution() {
+
+	int idx = 0;
+	
+	for (int i = 0; i < plus_num; i++) {
+		v.push_back(1);
+	}
+
+	for (int i = 0; i < minus_num; i++) {
+		v.push_back(2);
+	}
+
+	for (int i = 0; i < °öÇÏ±â_num; i++) {
+		v.push_back(3);
+	}
+
+	for (int i = 0; i < ³ª´©±â_num; i++) {
+		v.push_back(4);
+	}
+
+	sort(v.begin(), v.end());
+
+	do {
+		mini_solution(v);
+	} while (next_permutation(v.begin(), v.end()));
+
+	sort(answers.begin(), answers.end());
+	cout << answers[answers.size() - 1] << "\n";
+	cout << answers[0];
+}
+
+void input() {
+
+	cin >> n;
+
+	cin >> first_num;
+	for (int i = 0; i < n - 1; i++) {
+		cin >> arr[i];
+	}
+
+	cin >> plus_num >> minus_num >> °öÇÏ±â_num >> ³ª´©±â_num;
+}
 int main() {
+	
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		int k;
-		cin >> k;
-		num.push_back(k);
-	}
-
-	for (int i = 0; i < 4; i++) {
-		int k;
-		cin >> k;
-		if (i == 0) {
-			for (int j = 0; j < k; j++) {
-				operatorr.push_back(1);
-			}
-		}
-
-		else if (i == 1) {
-			for (int j = 0; j < k; j++) {
-				operatorr.push_back(2);
-			}
-		}
-		
-		else if (i == 2) {
-			for (int j = 0; j < k; j++) {
-				operatorr.push_back(3);
-			}
-		}
-
-		else {
-			for (int j = 0; j < k; j++) {
-				operatorr.push_back(4);
-			}
-		}
-	}
-
-	sort(operatorr.begin(), operatorr.end());
-
-	int result;
-	int idx = 0;
-
-	do {
-
-		idx = 0;
-		for (int i = 0; i < operatorr.size(); i++) {
-	
-			if (i == 0) {
-				if (operatorr[i] == 1) {
-					result = num[0] + num[1];
-					idx++;
-				}
-
-				else if (operatorr[i] == 3) {
-					result = num[0] * num[1];
-					idx++;
-				}
-
-				else if (operatorr[i] == 2) {
-					result = num[0] - num[1];
-					idx++;
-				}
-
-				else if (operatorr[i] == 4) {
-					result = num[0] / num[1];
-					idx++;
-				}
-			}
-
-			else {
-				if (operatorr[i] == 3) {
-					result = result * num[idx + 1];
-					idx++;
-				}
-
-				else if (operatorr[i] == 1) {
-					result = result + num[idx + 1];
-					idx++;
-				}
-
-				else if (operatorr[i] == 2) {
-					result = result - num[idx + 1];
-					idx++;
-				}
-
-				else if (operatorr[i] == 4) {
-					result = result / num[idx + 1];
-					idx++;
-				}
-			}		
-		}
-
-		results.push_back(result);
-
-	} while (next_permutation(operatorr.begin(), operatorr.end()));
-	
-	sort(results.begin(), results.end());
-
-	cout << results[results.size() - 1] << "\n";
-	cout << results[0] << "\n";
+	input();
+	solution();
 }
