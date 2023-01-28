8ca9eee35fb53dc92e344490714e4367a38b43d7
youbeenJeong <youbeen2798@naver.com>
2023-01-28 23:17:39 +0900

    Add files via upload

--- 14891_톱니바퀴.cpp
+++ 14891_톱니바퀴.cpp
@@ -1,170 +1,127 @@
 #include <iostream>
 #include <deque>
+#include <vector>
+
 using namespace std;
 
-int rotatedirection[5]; //각각의 톱니바퀴를 회전시킨다는 가정하에 회전 방향
-
-bool rotateornot[5];
-
-deque <char> d1;
-deque <char> d2;
-deque <char> d3;
-deque <char> d4;
-
 int k;
+int arr[5][9];
 
-void isrotate(int num) { //회전시킬지 말지 결정
+deque<int> d[5];
 
-	if (num == 1) { //첫번째 톱니바퀴를 회전시키면
-		rotateornot[1] = true;
-		if (d1[2] == d2[6]) {
-			rotateornot[2] = false;
-			return;
-		}
-		else {
-			rotateornot[2] = true;
-			if (d2[2] == d3[6]) {
-				rotateornot[3] = false;
-				return;
-			}
-			else {
-				rotateornot[3] = true;
-				if (d3[2] == d4[6]) {
-					rotateornot[4] = false;
-				}
-				else {
-					rotateornot[4] = true;
-					return;
-				}
-			}
-		}
+vector<pair<int, int>> v; //회전시킨 톱니바퀴 번호, 방향
+
+void rotate(int num, int dir) {
+
+	if (dir == -1) { //시계방향
+		int number = d[num].front(); //첫번째 숫자
+		d[num].pop_front();
+		d[num].push_back(number);
 	}
-
-	if (num == 2) {
-		rotateornot[2] = true;
-		if (d1[2] == d2[6]) {
-			rotateornot[1] = false;
-		}
-		else {
-			rotateornot[1] = true;
-		}
-
-		if (d2[2] == d3[6]) {
-			rotateornot[3] = false;
-			return;
-		}
-		else {
-			rotateornot[3] = true;
-			if (d3[2] == d4[6]) {
-				rotateornot[4] = false;
-				return;
-			}
-			else {
-				rotateornot[4] = true;
-			}
-		}
-	}
-
-	if (num == 3) {
-		rotateornot[3] = true;
-
-		if (d3[2] == d4[6]) {
-			rotateornot[4] = false;
-		}
-		else {
-			rotateornot[4] = true;
-		}
-
-		if (d2[2] == d3[6]) {
-			rotateornot[2] = false;
-			return;
-		}
-		else {
-			rotateornot[2] = true;
-			if (d1[2] == d2[6]) {
-				rotateornot[1] = false;
-				return;
-			}
-			else {
-				rotateornot[1] = true;
-				return;
-			}
-		}
-	}
-
-	if (num == 4) {
-		rotateornot[4] = true;
-
-		if (d3[2] == d4[6]) {
-			rotateornot[3] = false;
-			return;
-		}
-		else {
-			rotateornot[3] = true;
-			if (d2[2] == d3[6]) {
-				rotateornot[2] = false;
-				return;
-			}
-			else {
-				rotateornot[2] = true;
-				if (d1[2] == d2[6]) {
-					rotateornot[1] = false;
-				}
-				else {
-					rotateornot[1] = true;
-				}
-			}
-		}
+	else { //반시계방향
+		int number = d[num].back(); //마지막 숫자
+		d[num].pop_back();
+		d[num].push_front(number);
 	}
 }
 
-void startrotate() { //회전시키기
+void mini_solution(int num, int dir) {
 
-	if (rotateornot[1]) {
-		if (rotatedirection[1] == 1) { //시계방향
-			d1.push_front(d1.back());
-			d1.pop_back();
+	vector<pair<int, int>> v;
+	v.push_back({ num, dir });
+
+	if (num == 1) { //1번 톱니바퀴를 회전시키면
+
+		for (int i = 2; i <= 4; i++) {
+			if (d[i - 1][2] != d[i][6]) { //서로 극이 다르다면
+				dir *= -1;
+				v.push_back({ i,dir });
+				continue;
+			}
+			break;
 		}
-		else {
-			d1.push_back(d1.front());
-			d1.pop_front();
+	}
+	else if (num == 2) { //2번 톱니바퀴를 회전시키면
+
+		if (d[1][2] != d[2][6]) { //1번 바퀴와 상극이라면
+			v.push_back({ 1, dir * -1 }); 
+		}
+		if (d[2][2] != d[3][6]) { //3번 바퀴와 상극이라면
+			v.push_back({ 3, dir * -1 });
+
+			if (d[3][2] != d[4][6]) { //4번 바쿠와 상극이라면
+				v.push_back({ 4,dir });
+			}
+		}
+	}
+	else if (num == 3) { //3번 톱니바퀴를 회전시키면
+
+		if (d[2][2] != d[3][6]) { //2번 바퀴와 상극이라면
+			v.push_back({ 2, dir * -1 });
+			if (d[1][2] != d[2][6]) {
+				v.push_back({ 1, dir });
+			}
+		}
+		
+		if (d[3][2] != d[4][6]) { //4번 바퀴와 상극이면
+			v.push_back({ 4,dir * -1 }); 
+		}
+	}
+	else if (num == 4) { //4번 톱니바퀴를 회전시키면
+
+		for (int i = 3; i >= 1; i--) {
+			if (d[i][2] != d[i + 1][6]) {
+				dir *= -1;
+				v.push_back({ i,dir });
+				continue;
+			}
+			break;
 		}
 	}
 
-	if (rotateornot[2]) {
-		if (rotatedirection[2] == 1) { //시계방향
-			d2.push_front(d2.back());
-			d2.pop_back();
-		}
-		else {
-			d2.push_back(d2.front());
-			d2.pop_front();
+	for (int i = 0; i < v.size(); i++) {
+		rotate(v[i].first, v[i].second);
+	}
+}
+
+int get_score() {
+	int score = 0;
+
+	
+	if (d[1][0] == 1) {
+		score += 1;
+	}
+	if (d[2][0] == 1) {
+		score += 2;
+	}
+	if (d[3][0] == 1) {
+		score += 4;
+	}
+	if (d[4][0] == 1) {
+		score += 8;
+	}
+	return score;
+}
+void input() {
+	for (int i = 1; i <= 4; i++) {
+		string s;
+		cin >> s;
+
+		for (int j = 0; j < 8; j++) {
+			int num = s[j] - '0';
+			d[i].push_back(num);
 		}
 	}
 
-	if (rotateornot[3]) {
-		if (rotatedirection[3] == 1) {  //시계방향
-			d3.push_front(d3.back());
-			d3.pop_back();
-		}
-		else {
-			d3.push_back(d3.front());
-			d3.pop_front();
-		}
+	cin >> k;
+
+	for (int i = 0; i < k; i++) {
+		int num, dir;
+		cin >> num >> dir;
+		mini_solution(num, dir);
 	}
 
-	if (rotateornot[4]) {
-		if (rotatedirection[4] == 1) {  //시계방향
-			d4.push_front(d4.back());
-			d4.pop_back();
-		}
-		else {
-			d4.push_back(d4.front());
-			d4.pop_front();
-		}
-	}
-
-
+	cout << get_score();
 }
 
 int main() {
@@ -172,75 +129,5 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
-	for (int i = 0; i < 8; i++) {
-		char num;
-		cin >> num;
-		d1.push_back(num);
-	}
-
-	for (int i = 0; i < 8; i++) {
-		char num;
-		cin >> num;
-		d2.push_back(num);
-	}
-
-	for (int i = 0; i < 8; i++) {
-		char num;
-		cin >> num;
-		d3.push_back(num);
-	}
-
-	for (int i = 0; i < 8; i++) {
-		char num;
-		cin >> num;
-		d4.push_back(num);
-	}
-
-	cin >> k;
-
-	for (int i = 0; i < k; i++) {
-		int num, direction;
-		cin >> num >> direction; //회전시킬 톱니의 번호와 방향
-
-		if (num % 2 != 0) {
-			rotatedirection[1] = direction;
-			rotatedirection[3] = direction;
-
-			rotatedirection[2] = -1 * direction;
-			rotatedirection[4] = -1 * direction;
-		}
-		else {
-			rotatedirection[1] = -1 * direction;
-			rotatedirection[3] = -1 * direction;
-
-			rotatedirection[2] = direction;
-			rotatedirection[4] = direction;
-		}
-
-		for (int i = 0; i < 5; i++) {
-			rotateornot[i] = false;
-		}
-
-		isrotate(num);
-
-		startrotate();
-
-	}
-
-	int answer = 0;
-	if (d1[0] == '1') {
-		answer += 1;
-	}
-	if (d2[0] == '1') {
-		answer += 2;
-	}
-	if (d3[0] == '1') {
-		answer += 4;
-	}
-	if (d4[0] == '1') {
-		answer += 8;
-	}
-
-	cout <<  answer;
-
+	input();
 }
