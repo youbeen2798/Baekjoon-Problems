7cf1ea27abcecac58bde05d63b72b4d06c522f0c
youbeenJeong <youbeen2798@naver.com>
2023-02-01 09:18:33 +0900

    Add files via upload

--- 15652_N과 M(4).cpp
+++ 15652_N과 M(4).cpp
@@ -1,31 +1,28 @@
-//자연수 N과 M이 주어졌을 때, 길이가 M인 수열을 모두 출력하는 프로그램
-// 단, 아래 조건을 만족해야함
-
-//조건 1. 1부터 N까지 자연수 중에서 M개를 고른 수열
-//조건 2. 같은 수를 여러 번 골라도 됨
-//조건 3. 고른 수열은 비내림차순
-
 #include <iostream>
 
 using namespace std;
 
 int n, m;
 int d = 1;
-int arr[20];
+int arr[9];
 
-void func(int k) {
+void input() {
+	cin >> n >> m;
+}
+
+void func(int k){
 	if (k == m) {
-		for (int i = 0; i < k; i++) {
-			cout << arr[i] << ' ';
+		for (int i = 0; i < m; i++) {
+			cout << arr[i] << " ";
 		}
 		cout << "\n";
+		return;
 	}
-	else {
-		for (int i = d; i <= n; i++) {
-				arr[k] = i;
-				d = i;
-				func(k + 1);
-		}
+
+	for (int i = d; i <= n; i++) {
+		arr[k] = i;
+		d = i;
+		func(k + 1);
 	}
 }
 
@@ -34,7 +31,6 @@ int main() {
 	cin.tie(0);
 	cout.tie(0);
 
-	cin >> n >> m;
-
+	input();
 	func(0);
 }
