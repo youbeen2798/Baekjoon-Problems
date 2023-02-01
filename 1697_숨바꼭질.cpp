71e036a75abe5d9a149bef1937f7c79c59eb583d
youbeenJeong <youbeen2798@naver.com>
2023-02-01 11:04:57 +0900

    Add files via upload

--- 1697_숨바꼭질.cpp
+++ 1697_숨바꼭질.cpp
@@ -1,37 +1,47 @@
 #include <iostream>
 #include <queue>
+using namespace std;
 
 using namespace std;
 
 int n, k;
-void input() {
-	cin >> n >> k;
-}
+bool visited[100001];
+
 
 void solution() {
-	queue<pair<int, int>> q;
+	queue<pair<int,int>> q;
 	q.push({ n,0 });
+
+	visited[n] = true;
 
 	while (!q.empty()) {
 		int a = q.front().first;
 		int cnt = q.front().second;
-		
-		q.pop();
+
+	//	cout << "a: " << a << "\n";
 		if (a == k) {
-			cout << cnt;
+			cout <<  cnt << "\n";
 			break;
 		}
-		if (0 <= a + 1 && a + 1 <= 100000) {
+		q.pop();
+
+		if (!visited[a - 1] && a -1 >= 0 && a - 1 <= 100000) {
+			q.push({ a - 1, cnt + 1 });
+			visited[a - 1] = true;
+		}
+		if (!visited[a + 1] && a + 1 >= 0 && a + 1 <= 100000) {
 			q.push({ a + 1, cnt + 1 });
+			visited[a + 1] = true;
 		}
-		if (0 <= a - 1 && a - 1 <= 100000) {
-			q.push({ a - 1, cnt + 1 }); 
-		}
-		if (0 <= 2 * a && 2 * a <= 100000) {
+		if (!visited[2 * a] && 2 * a >= 0 && 2 * a <= 100000) {
 			q.push({ 2 * a, cnt + 1 });
+			visited[2 * a] = true;
 		}
 	}
+}
 
+void input() {
+	cin >> n >> k;
 }
 int main() {
 	ios_base::sync_with_stdio(0);
