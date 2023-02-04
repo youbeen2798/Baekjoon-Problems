cc7326714e042cd447ac4b3bf94acaf32a8f76dc
youbeenJeong <youbeen2798@naver.com>
2023-02-05 01:37:32 +0900

    Add files via upload

--- 11057_오르막 수.cpp
+++ 11057_오르막 수.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
-#define N 10007
 
 using namespace std;
 
-int dp[1001][10];
 int n;
-
-void solution() {
-
-	for (int i = 0; i <= 9; i++) {
-		dp[1][i] = 1;
-	}
-
-	for (int i = 1; i <= n; i++) {
-		dp[i][0] = 1;
-	}
-
-	for (int i = 2; i <= n; i++) {
-		for (int j = 1; j <= 9; j++) {
-			dp[i][j] = (dp[i][j - 1] + dp[i - 1][j]) % N;
-		}
-	}
-
-	long ans = 0;
-	for (int i = 0; i <= 9; i++) {
-		ans += dp[n][i];
-	}
-
-	cout << ans % N;
-}
-
-void input() {
-	cin >> n;
-}
+int dp[1001][10];
 
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	input();
-	solution();
+	cin >> n;
+
+	for (int i = 0; i <= 9; i++) {
+		dp[1][i] = 1;
+	}
+
+	for (int i = 2; i <= n; i++) {
+		dp[i][0] = 1;
+		for (int j = 1; j <= 9; j++) {
+			dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+		}
+	}
+
+	int ans = 0;
+	for (int i = 0; i <= 9; i++) {
+		ans += dp[n][i];
+	}
+
+	cout << ans % 10007;
 }
