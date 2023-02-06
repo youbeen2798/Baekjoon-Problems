c358803e59031129f232d786b45f83eb16f51652
youbeenJeong <youbeen2798@naver.com>
2023-02-07 07:46:52 +0900

    Add files via upload

--- 1920_수 찾기.cpp
+++ 1920_수 찾기.cpp
@@ -1,45 +1,41 @@
 #include <iostream>
-#include <algorithm>
+#include <set>
 using namespace std;
 
-int a[100005];
-int n, m;
+int n;
+set<int> arr1;
+int m;
+set<int> arr2;
 
-int binarysearch(int target) {
-	int st = 0;
-	int en = n - 1;
-	while (st <= en) {
-		int mid = (st + en) / 2;
-		if (a[mid] < target) {
-			st = mid + 1;
-		}
-		else if (a[mid] > target) {
-			en = mid - 1;
-		}
-		else {
-			return 1;
-		}
+void solution() {
+
+}
+void input() {
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		int num;
+		cin >> num;
+		arr1.insert(num);
 	}
-	return 0;
+
+	cin >> m;
+
+	for (int i = 0; i < m; i++) {
+		int num;
+		cin >> num;
+		if (arr1.find(num) != arr1.end()) {
+			//원소가 있으면
+			cout << 1 << "\n";
+			continue; 
+		}
+		cout << 0 << "\n";
+	}
 }
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
-	cin >> n;
-
-	for (int i = 0; i < n; i++) {
-		cin >> a[i];
-	}
-
-	sort(a, a + n);
-
-	cin >> m;
-
-	while (m--) {
-		int t;
-		cin >> t;
-		cout << binarysearch(t) << "\n";
-	}
+	input();
 }
