211ba0787879a26ecbeb9affe0ac496f729fbdf0
youbeenJeong <youbeen2798@naver.com>
2023-03-08 15:58:15 +0900

    Add files via upload

--- 17779_게리맨더링2.cpp
+++ 17779_게리맨더링2.cpp
@@ -1,173 +1,166 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
-#include <cstring>
 
 using namespace std;
 
-int n, x, y;
+int n;
+int arr[21][21];
+int people_num[6];
+int total_people_num = 0;
+int x, y, d1, d2;
+int ans = 9999999;
 
-int board[21][21];
-bool divide[21][21];
+void one() {
 
-int group1 = 0;
-int group2 = 0;
-int group3 = 0;
-int group4 = 0;
-int group5 = 0;
+	people_num[1] = 0;
+	int now_x = x + d1;
+	int now_y = y - d1;
 
-int answer = 9999999;
+	for (int i = 0; i <= d1; i++) {
+		int x = now_x - i;
+		int y = now_y + i;
+		for (int r = x - 1; r >= 1; r--) {
+			people_num[1] += arr[r][y];
+		}
+	}
 
-void input() {
-	cin >> n; //격자 크기
 
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			cin >> board[i][j]; //격자 값
+	for (int c = 1; c < y - d1; c++) {
+		for (int r = 1; r < x + d1; r++) {
+			people_num[1] += arr[r][c];
 		}
 	}
 }
 
-void solve() {
+void two() {
 
-	for (int i = 1; i <= n - 2; i++) {
-		for (int j = 2; j <= n - 1; j++) {
+	people_num[2] = 0;
+
+	int now_x = x;
+	int now_y = y;
+
+	for (int i = 1; i <= d2; i++) {
+		int x = now_x + i;
+		int y = now_y + i;
+		for (int r = 1; r < x; r++) {
+			people_num[2] += arr[r][y];
+		}
+	}
+
+	for (int c = y + d2 + 1; c <= n; c++) {
+		for (int r = 1; r <= x + d2; r++) {
+			people_num[2] += arr[r][c];
+		}
+	}
+}
+
+void three() {
+
+	people_num[3] = 0;
+
+	int now_x = x + d1;
+	int now_y = y - d1;
+
+	for (int i = 0; i < d2; i++) {
+		int x = now_x + i;
+		int y = now_y + i;
+		for (int r = x + 1; r <= n; r++) {
+			people_num[3] += arr[r][y];
+		}
+	}
+
+	for (int i = now_x; i <= n; i++) {
+		for (int j = 1; j < now_y; j++) {
+			people_num[3] += arr[i][j];
+		}
+	}
+
+}
+
+void four() {
+
+	people_num[4] = 0;
+
+	int now_x = x + d1 + d2;
+	int now_y = y - d1 + d2;
+
+	for (int i = 0; i <= d1; i++) {
+		int x = now_x - i;
+		int y = now_y + i;
+
+		for (int r = x + 1; r <= n; r++) {
+			people_num[4] += arr[r][y];
+		}
+	}
+
+	
+	for (int j = y + d2 + 1; j <= n; j++) {
+		for (int i = x + d2 + 1; i <= n; i++) {
+			people_num[4] += arr[i][j];
+		}
+	}
+}
+
+void five() {
+
+	people_num[5] = total_people_num;
+
+	for (int i = 1; i <= 4; i++) {
+		people_num[5] -= people_num[i];
+	}
+}
+
+void check_diff() {
+
+	int mini = 9999999;
+	int maxi = 0;
+
+	for (int i = 1; i <= 5; i++) {
+		mini = min(mini, people_num[i]);
+		maxi = max(maxi, people_num[i]);
+	}
+
+	int diff = maxi - mini;
+
+	ans = min(diff, ans);
+
+
+}
+void check(int x1, int y1) {
+
+	for (int i = 1; i <= n - 1; i++) {
+		for (int j = 1; j <= n - 1; j++) {
+			d1 = i;
+			d2 = j;
+			one();
+			two();
+			three();
+			four();
+			five();
+			check_diff();
+		}
+	}
+
+}
+void solution() {
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
 			x = i;
 			y = j;
-			for (int d1 = 1; d1 <= y - 1; d1++) {
-				for (int d2 = 1; d2 <= n - y; d2++) {
-					if (d1 + d2 <= n - x) {
+			check(i, j);
+		}
+	}
 
-					}
-					else {
-						continue;
-					}
+}
 
-					vector <int> v;
-
-					memset(divide, false, sizeof(divide));
-					group1 = 0;
-					group2 = 0;
-					group3 = 0;
-					group4 = 0;
-					group5 = 0;
-
-					for (int r = x, c = y; r <= x + d1, c >= y - d1; r++, c--) {
-						divide[r][c] = true;
-					}
-
-					for (int r = x, c = y; r <= x + d2, c <= y + d2; r++, c++) {
-						divide[r][c] = true;
-					}
-
-					for (int r = x + d1, c = y - d1; r <= x + d1 + d2, c <= y - d1 + d2; r++, c++) {
-						divide[r][c] = true;
-					}
-
-					for (int r = x + d2, c = y + d2; r <= x + d1 + d2, c >= y + d2 - d1; r++, c--) {
-						divide[r][c] = true;
-					}
-
-					int start_x = x + d1 - 1;
-					int start_y = y - d1 + 1;
-
-					int end_x = x + d2 - 1;
-					int end_y = y + d2 - 1;
-
-					int mid_x = x;
-					int mid_y = y;
-
-
-
-					for (int k = mid_x, p = mid_y; k <= start_x, p >= start_y; k++, p--) {
-
-
-						int tmp = k;
-						while (true) {
-							int s = tmp + 1; //2
-							if (divide[s][p] == false) {
-								divide[s][p] = true;
-							}
-							else {
-								break;
-							}
-							tmp++;
-						}
-					}
-
-
-					for (int k = mid_x, p = mid_y; k <= end_x, p <= end_y; k++, p++) {
-
-						int tmp = k;
-						while (true) {
-							int s = tmp + 1; //2
-							if (divide[s][p] == false) {
-								divide[s][p] = true;
-							}
-							else {
-								break;
-							}
-							tmp++;
-						}
-					}
-
-
-					for (int k = 1; k <= n; k++) {
-						for (int p = 1; p <= n; p++) {
-							if (divide[k][p]) {
-								group5 += board[k][p];
-							}
-						}
-					}
-
-					v.push_back(group5);
-
-					for (int k = 1; k < x + d1; k++) {
-						for (int p = 1; p <= y; p++) {
-							if (!divide[k][p]) {
-								group1 += board[k][p];
-							}
-						}
-					}
-					v.push_back(group1);
-
-					for (int k = 1; k <= x + d2; k++) {
-						for (int p = y + 1; p <= n; p++) {
-							if (!divide[k][p]) {
-								group2 += board[k][p];
-							}
-						}
-					}
-					v.push_back(group2);
-
-
-					for (int k = x + d1; k <= n; k++) {
-						for (int p = 1; p < y - d1 + d2; p++) {
-							if (!divide[k][p]) {
-								group3 += board[k][p];
-							}
-						}
-					}
-					v.push_back(group3);
-
-					for (int k = x + d2 + 1; k <= n; k++) {
-						for (int p = y - d1 + d2; p <= n; p++) {
-							if (!divide[k][p]) {
-								group4 += board[k][p];
-							}
-						}
-					}
-					v.push_back(group4);
-
-
-					sort(v.begin(), v.end());
-
-
-					answer = min(answer, v[v.size() - 1] - v[0]);
-				}
-			}
-
-
+void input() {
+	cin >> n;
+	
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> arr[i][j];
+			total_people_num += arr[i][j];
 		}
 	}
 }
@@ -177,9 +170,9 @@ int main() {
 	cout.tie(0);
 
 	input();
-	solve();
 
-	cout << answer << "\n";
+	solution();
 
-
+	cout << ans;
+	
 }
