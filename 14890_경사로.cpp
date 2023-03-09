7de5638bed0cad7f704afb8b476e665681573383
youbeenJeong <youbeen2798@naver.com>
2023-03-09 14:07:44 +0900

    Add files via upload

--- 14890_경사로.cpp
+++ 14890_경사로.cpp
@@ -2,189 +2,184 @@
 
 using namespace std;
 
-int n, l;
-int board[101][101];
-bool userunway[101][101];
-int answer = 0;
+int n;
+int l;
+int arr[101][101];
+int row_arr[101];
+int col_arr[101];
+bool row_saro_exists[101][101];
+bool col_saro_exists[101][101];
 
-void row_solve() {
-
-	memset(userunway, false, sizeof(userunway));
+void not_need_gyoungsaro_row() {
+	//행일때
+	//경사로가 필요하지 않을 때
 
 	for (int i = 0; i < n; i++) {
-		bool possible = true;
-		for (int j = 0; j < n-1; j++) {
-
-			if (board[i][j] != board[i][j + 1]) {
-				int diff = board[i][j + 1] - board[i][j];
-				if (abs(diff) > 1) {
-					possible = false;
-					break;
-				}
-				else {
-
-					if (diff == 1) { //높이가 1차이로 올라갈 때
-
-						if (userunway[i][j]) { //차이가 다른데 해당 행렬이 경사로를 사용하면
-							possible = false;
-							break;
-						}
-						int height = board[i][j];
-						for (int k = j; k >= j - l + 1; k--) {
-							if (k >= 0) {
-								if (board[i][k] != height || userunway[i][k] == true) { //다른 행렬이 경사로를 사용하면
-									possible = false;
-									break;
-								}
-							}
-							else {
-								possible = false;
-								break;
-							}
-						}
-						if (possible) {
-							for (int k = j; k >= j - l + 1; k--) {
-								userunway[i][k] = true;
-							}
-						}
-						else {
-							break;
-						}
-
-					}
-					else if (diff == -1) { //높이가 1차이로 내려갈 때
-						int height = board[i][j+1];
-						for (int k = j + 1; k < j + l + 1; k++) { //등호 빼야할듯
-							if (k < n) {
-								if (board[i][k] != height || userunway[i][k] == true) {
-									possible = false;
-									break;
-								}
-							}
-							else {
-								possible = false;
-								break;
-							}
-						}
-
-						if (possible) {
-							for (int k = j; k < j + l + 1; k++) { //등호 빼야할 듯
-								userunway[i][k] = true;
-							}
-						}
-						else {
-							break;
-						}
-					}
-
-					
-				}
+		bool can_walk = true;
+		for (int j = 0; j < n - 1; j++) {
+			if (arr[i][j] != arr[i][j + 1]) {
+				can_walk = false;
+				break;
 			}
-
-
-
 		}
-		if (possible) {
-			answer++;
+		if (can_walk) {
+			row_arr[i] = true;
 		}
 	}
 }
 
-void col_solve() {
-
-	memset(userunway, false, sizeof(userunway));
+void not_need_gyoungsaro_col() {
+	//열일때
+	//경사로가 필요하지 않을 때
 
 	for (int j = 0; j < n; j++) {
-		bool possible = true;
+		bool can_walk = true; 
 		for (int i = 0; i < n - 1; i++) {
-			if (board[i][j] != board[i+1][j]) {
-				int diff = board[i+1][j] - board[i][j];
-				if (abs(diff) > 1) {
-					possible = false;
-					break;
-				}
-				else {
-					if (diff == 1) { //높이가 1차이로 올라갈 때
-						
-						if (userunway[i][j]) { //차이가 다른데 해당 행렬이 경사로를 사용하면
-							possible = false;
-							break;
-						}
-						int height = board[i][j];
-
-						for (int k = i; k >= i - l + 1; k--) {
-							if (k >= 0) {
-								if (board[k][j] != height || userunway[k][j] == true) { //다른 행렬이 경사로를 사용하면
-									possible = false;
-									break;
-								}
-							}
-							else {
-								possible = false;
-								break;
-							}
-						}
-
-						if (possible) {
-							for (int k = i; k >= i - l + 1; k--) {
-								userunway[k][j] = true;
-							}
-						}
-						else {
-							break;
-						}
-
-					}
-					else if (diff == -1) { //높이가 1차이로 내려갈 때
-						int height = board[i+1][j];
-						for (int k = i + 1; k < i + l + 1; k++) {
-							if (k < n) {
-								if (board[k][j] != height || userunway[k][j] == true) {
-									possible = false;
-									break;
-								}
-							}
-							else {
-								possible = false;
-								break;
-							}
-						}
-
-						if (possible) {
-							for (int k = i + 1; k < i + l + 1; k++) {
-								userunway[k][j] = true;
-							}
-						}
-						else {
-							break;
-						}
-					}
-
-				}
+			if (arr[i][j] != arr[i + 1][j]) {
+				can_walk = false;
+				break;
 			}
 		}
-		if (possible) {
-			answer++;
+		if (can_walk) {
+			col_arr[j] = true;
 		}
 	}
 }
-void input() {
-	cin >> n >> l;
+void not_need_gyoungsaro() {
+
+	not_need_gyoungsaro_row();
+	not_need_gyoungsaro_col();
+}
+
+void need_gyoungsaro_row() {
+	//행일때
+	//경사로가 필요할 때
+
+	//왼쪽에서 오른쪽으로
+	for (int i = 0; i < n; i++) {
+		bool can_walk = true;
+		for (int j = 0; j < n - 1; j++) {
+			//올라가면
+			if (arr[i][j] + 1 == arr[i][j + 1]) {
+				for (int k = j - l + 1; k <= j; k++) {
+					if (0 <= k && k < n && arr[i][j] == arr[i][k] && !col_saro_exists[i][k]) {
+						col_saro_exists[i][k] = true;
+						continue;
+					}
+					can_walk = false;
+					break;
+				}
+			}
+			//내려가면
+			else if (arr[i][j] - 1 == arr[i][j + 1]) {
+				for (int k = j + 1; k <= j + l; k++) {
+					if (0 <= k && k < n && arr[i][j + 1] == arr[i][k] && !col_saro_exists[i][k]) {
+						col_saro_exists[i][k] = true;
+						continue;
+					}
+					can_walk = false;
+					break;
+				}
+			}
+			//차이가 1이상 차이 나면
+			else if (abs(arr[i][j] - arr[i][j + 1]) > 1) {
+				can_walk = false;
+				break;
+			}
+
+			if (!can_walk) {
+				break;
+			}
+		}
+		if (can_walk) {
+			row_arr[i] = true;
+		}
+	}
+}
+
+void need_gyoungsaro_col() {
+	//열일때
+	//경사로가 필요할 때
+
+	for (int j = 0; j < n; j++) {
+		bool can_walk = true;
+		for (int i = 0; i < n - 1; i++) {
+			//올라가면
+			if (arr[i][j] + 1 == arr[i + 1][j]) {
+				for (int k = i - l + 1; k <= i; k++) {
+					if (0 <= k && k < n && arr[i][j] == arr[k][j] && !row_saro_exists[k][j]) {
+						row_saro_exists[k][j] = true;
+						continue;
+					}
+					can_walk = false;
+					break;
+				}
+			}
+			//내려가면
+			else if (arr[i][j] - 1 == arr[i + 1][j]) {
+				for (int k = i + 1; k <= i + l; k++) {
+					if (0 <= k && k < n && arr[i + 1][j] == arr[k][j] && !row_saro_exists[k][j] == true) {
+						row_saro_exists[k][j] = true;
+						continue;
+					}
+					can_walk = false;
+					break;
+				}
+			}
+			//차이가 1이상 차이 나면
+			else if (abs(arr[i][j] - arr[i + 1][j]) > 1) {
+				can_walk = false;
+				break;
+			}
+
+			if (!can_walk) {
+				break;
+			}
+		}
+		if (can_walk) {
+			col_arr[j] = true;
+		}
+	}
+}
+void need_gyoungsaro() {
+	need_gyoungsaro_row();
+	need_gyoungsaro_col();
+}
+
+void print() {
+
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		ans += row_arr[i];
+		ans += col_arr[i];
+	}
 
 	for (int i = 0; i < n; i++) {
+	}
+
+	cout << ans;
+}
+void solution() {
+
+	not_need_gyoungsaro();
+	need_gyoungsaro();
+	print();
+}
+void input() {
+
+	cin >> n >> l; 
+
+	for(int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
-			cin >> board[i][j];
+			cin >> arr[i][j];
 		}
 	}
 }
-
 int main() {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
 
 	input();
-	row_solve();
-	col_solve();
-	cout << answer;
+	solution();
 }
