6dc2ab6c2bc0799bff5ff00547b1f2efd9d4eeb9
youbeenJeong <youbeen2798@naver.com>
2023-04-26 00:48:01 +0900

    Add files via upload

--- 15686_치킨배달.cpp
+++ 15686_치킨배달.cpp
@@ -1,46 +1,56 @@
 #include <iostream>
 #include <vector>
-#include <set>
-
 using namespace std;
 
-int n; //도시 크기
-int m; //치킨
-int arr[51][51];
-int ans = 99999;
+int n;
+int m;
+int map[51][51];
+//0: 빈칸, 1: 집, 2: 치킨집
 
 vector<pair<int, int>> chickens;
-set<pair<int, int>> homes;
+vector<pair<int, int>> homes;
+int ans = 9999999;
 
-void check_dist(vector<pair<int, int>> chickens) {
+void check_diff(vector<pair<int, int>> selected_chickens) {
 
-	int tmp_total_min_dist = 0;
+	int tmp_ans = 0; 
+	for (int i = 0; i < homes.size(); i++) {
+		int home_x = homes[i].first;
+		int home_y = homes[i].second;
 
-	for (auto home : homes) {
-		int min_dist = 99999;
+		int min_dist_diff = 99999;
+		for (int j = 0; j < selected_chickens.size(); j++) {
+			int chicken_x = selected_chickens[j].first;
+			int chicken_y = selected_chickens[j].second;
 
-		for (int i = 0; i < chickens.size(); i++) {
-			int dist = abs(home.first - chickens[i].first) + abs(home.second - chickens[i].second);
-			min_dist = min(min_dist, dist);
+			int diff = abs(home_x - chicken_x) + abs(home_y - chicken_y);
+			min_dist_diff = min(min_dist_diff, diff);
 		}
-		tmp_total_min_dist += min_dist;
+		tmp_ans += min_dist_diff;
 	}
 
-	ans = min(ans, tmp_total_min_dist);
+	ans = min(ans, tmp_ans);
 }
-void combination(vector<pair<int, int>> arr, vector<pair<int, int>> comb, int r, int depth, int idx) {
-
+void combination(vector<pair<int, int>> arr, vector<pair<int, int>> comb, int r, int idx, int depth) {
+	
 	if (r == 0) {
-		check_dist(comb);
+		check_diff(comb);
+		/*
+		cout << "###########" << "\n";
+		for (int i = 0; i < comb.size(); i++) {
+			cout << comb[i].first << " " << comb[i].second << "\n";
+		}
+		*/
 	}
 	else if (depth == arr.size()) {
 		return;
 	}
 	else {
 		comb[idx] = arr[depth];
-		combination(arr, comb, r - 1, depth + 1, idx + 1);
 
-		combination(arr, comb, r, depth + 1, idx);
+		combination(arr, comb, r - 1, idx + 1, depth + 1);
+
+		combination(arr, comb, r, idx, depth + 1);
 	}
 }
 void solution() {
@@ -50,26 +60,26 @@ void solution() {
 	combination(chickens, comb, m, 0, 0);
 }
 void input() {
+
 	cin >> n >> m;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			cin >> arr[i][j];
-			if (arr[i][j] == 2) {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 2) {
 				chickens.push_back({ i,j });
 			}
-			else if (arr[i][j] == 1) {
-				homes.insert({ i,j });
+			else if (map[i][j] == 1) {
+				homes.push_back({ i,j });
 			}
 		}
 	}
 }
 int main() {
-
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	
+
 	input();
 	solution();
 	cout << ans;
